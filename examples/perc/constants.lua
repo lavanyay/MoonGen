444eaa3b346d28a887b4d229f8b92b442e68303d
Lavanya Jose <lavanyaj@cs.stanford.edu>
2016-08-04 16:49:03 +0000

    special queues and other constants

---
+++ examples/perc/constants.lua
@@ -0,0 +1,10 @@
+constantsMod = {
+   ["DATA_RXQUEUE"] = 0,
+   ["CONTROL_QUEUE"] = 1,
+   ["FINACK_QUEUE"] = 2,
+   ["DROP_QUEUE"] = 3,
+   ["ETHTYPE_FINACK"] = 5678,
+   ["ETHTYPE_DATA"] = 6789
+}
+
+return constantsMod
